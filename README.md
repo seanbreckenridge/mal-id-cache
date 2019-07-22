0cacab9bf645bcdb2bb637e30d0965cd68d9a618
Sean Breckenridge <seanbrecke@gmail.com>
2019-07-21 22:35:23 -0700

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+# mal-id-cache
+
+[cache.json](./cache.json) is a cache of all current [MAL](https://myanimelist.net/) (MyAnimeList) anime ids.
+
+The JSON file is structured like:
+
+```
+{
+    "sfw": [
+        1,
+        5,
+        ....
+
+    ],
+    "nsfw": [
+        188,
+        203,
+        ...    
+    ]
+}
+```
+
+Since MAL's IDs are not contiguous, and the "Just Added" page doesn't necessarily list the most recently added - just the latest IDs, sometimes newly approved anime will appear a couple pages in. This makes it somewhat annoying to maintain a cache of MAL IDs.
+
+The most obvious application for this cache is to use the cache to choose an entry at random.
+
+This is not meant to be a cache of all the data on MAL, it only lists IDs and whether the entry is SFW/NSFW.
+
+This will be updated whenever a new entry is added.
+
+##### Implementation Notes
+
+This checks ranges of the Just Added page periodically, ordered by most recent ID
+
+It checks the first 2 pages every ~30 minutes, the first 8 every 8 hours, the first 20 every 2 days and completely deletes and re-creates the cache every 15 days (incase entries are deleted or merged).
+
+If it finds an entry on any page, it extends how far it searches `(by 5 + (current_page / 5)`.
+
+### Installation
+
+If for some reason, you'd like to set up your own instance:
+
+To setup the cache, we have to use a local [Jikan](https://github.com/jikan-me/jikan) instance since the remote one would cache requests for too long. See [here](https://github.com/jikan-me/jikan-rest) for how to set that up.
+
+After installing initial dependencies:
+
+```
+git clone https://github.com/seanbreckenridge/mal-id-cache
+cd mal-id-cache
+pipenv install
+git clone https://github.com/jikan-me/jikan-rest
+cp env.dist jikan-rest/.env
+cd jikan-rest
+composer install
+php artisan key:generate
+```
+
+Start the server: `php -S localhost:8000 -t jikan-rest/public >> logs/jikan-requests.log 2>&1`
+
+`pipenv shell` to enter the virtualenv
+
+Initialize the cache (only required if you deleted `cache.json`): `python3 generate.py init`
+
+Keep the cache updated: `python3 generate.py run`
+
+If you're trying to debug, you can modify the jikan requests last in cache by modifying the `CACHE_EXPIRE` variables in [env.dist](./env.dist), or delete the cache for `jikan-rest` by doing:
+
+`rm -rf jikan-rest/storage/framework/cache/*`
---
+++ generate.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+
+import os
+import math
+import time
+import random
+import json
+import logging
+
+import backoff
+import click
+import pickledb
+import jikanpy
+from git import Repo
+
+# setup global path/objects and logging
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+logs_dir = os.path.join(root_dir, 'logs')
+if not os.path.exists(logs_dir):
+    os.makedirs(logs_dir)
+db_file = os.path.join(root_dir, 'cache.json')
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(os.path.join(logs_dir, "cache.log"))
+# format
+formatter = logging.Formatter(
+    "%(asctime)s %(levelno)s %(process)d %(message)s")
+fh.setFormatter(formatter)
+# log to stderr
+logger.addHandler(logging.StreamHandler())
+# and to the log file
+logger.addHandler(fh)
+j = jikanpy.Jikan("http://localhost:8000/v3/")
+
+
+class IndentedPickleDB(pickledb.PickleDB):
+
+    # Make sure lines are indented so that git diff's are small
+    def dump(self):
+        with open(self.loco, 'wt') as loco_f:
+            loco_f.write(json.dumps(self.db, indent=4))
+        return True
+
+    # sort the list
+    def lsort(self, name):
+        self.db[name].sort()
+        self.dump()
+
+
+db = IndentedPickleDB(location=db_file, auto_dump=False, sig=True)
+
+# there are less nsfw entries so we dont need to check as many pages
+nsfw_div_factor = 3
+
+
+class req_range:
+    """Since we extend whenever we find an entry, these aren't incredibly important
+    Most entries are added in the 0-4 page range, and theres a huge addition of entries
+    the page_range extensions will catch anything past ~20/30
+    """
+    class two:
+        val = 2
+        time = 60 * 30  # 30 minutes
+
+    class eight:
+        val = 8
+        time = 60 * 30 * 8  # 8 hours
+        last = 0
+
+    class twenty:
+        val = 20
+        time = 60 * 30 * 24 * 2  # 2 days
+        last = 0
+
+    class all:
+        val = "all"
+        time = 60 * 30 * 24 * 15 # 15 days
+        last = 0
+
+
+def backoff_hdlr(details):
+    logger.debug("Backing off {wait:0.1f} seconds afters {tries} tries " +
+                 "calling function {target} for with args {args} and kwargs " +
+                 "{kwargs}".format(**details))
+
+
+@backoff.on_exception(
+    backoff.fibo,  # fibonacci sequence backoff
+    (jikanpy.exceptions.JikanException,
+     jikanpy.exceptions.APIException),
+    max_tries=10,
+    on_backoff=backoff_hdlr)
+def get_search_page(p, nsfw):
+    resp = j.search(
+        'anime',
+        '',  # query string
+        page=p,
+        parameters={
+            'genre': 12,
+            'genre_exclude': 1 if nsfw else 0,
+            'order_by': 'id',
+            'sort': 'desc'})
+    # time.sleep(1) # default wait
+    return resp
+
+
+@click.group()
+def cli():
+    pass
+
+
+def update_req_range(found_new_entries: bool,
+                     current_page: int, page_range: int, *, is_nsfw: bool):
+    """Helper for updating the page range. If page range is higher, increases the amount we extend by"""
+    if found_new_entries:
+        extend_by = 3 + int(current_page / 5)
+        page_range_before_extend = page_range
+        if current_page + extend_by > page_range:
+            page_range = current_page + extend_by
+        if page_range != page_range_before_extend:
+            logger.debug(
+                "[{}]Extending search from {} to {}".format(
+                    "NSFW" if is_nsfw else "SFW",
+                    page_range_before_extend,
+                    math.ceil(page_range / nsfw_div_factor) if is_nsfw else page_range))
+    return page_range
+
+
+def add_from_page(type, page):
+    """If any new entries exist, adds them from the 'page' for the 'type' ('sfw'/'nsfw')
+    for that page.
+
+    returns the number of results on the page (typically 50), and if we found new entries
+    """
+    found_new_entries = False
+    prev_cache = db.lgetall(type)
+    results = get_search_page(
+        page, nsfw=True if type == 'nsfw' else False)['results']
+    for r in results:
+        if r["mal_id"] not in prev_cache:
+            found_new_entries = True
+            logger.info(
+                f"Found new entry (id:{r['mal_id']}, {r['title']}) on page {page}")
+            db.ladd(type, r['mal_id'])
+    db.dump()
+    return len(results), found_new_entries
+
+
+@cli.command(
+    name="run", help="Periodically check if new entries exist. Runs until stopped")
+def loop():
+
+    for type in ['sfw', 'nsfw']:
+        if db.get(type) == False:
+            db.lcreate(type)
+
+    while True:
+        # Initialize 'state' values to 0, (i.e. its Jan 1. 1970, so if needed,
+        # req_range 2/8/20 are checked
+        req_range.eight.last = req_range.twenty.last = req_range.all.last = 0
+
+        # assume we're checking 2 pages
+        req_type = req_range.two
+        page_range = req_type.val
+
+        logger.debug("[loop] Checking state")
+        # read times from 'state' file
+        if os.path.exists("state"):
+            with open("state", "r") as last_scraped:
+                req_range.eight.last, req_range.twenty.last, req_range.all.last = list(
+                    map(int, last_scraped.read().strip().splitlines()))
+
+        # check if we should be checking more than 2 pages
+        for rt in [req_range.eight, req_range.twenty, req_range.all]:
+            if int(time.time() - rt.last) > rt.time:
+                req_type = rt
+                page_range = rt.val
+
+        logger.debug("[loop] checking {} pages".format(page_range))
+
+        # delete cache if we're checking for deleted/merged entries
+        if req_type == req_range.all:
+            db.db['sfw'] = []
+            db.db['nsfw'] = []
+            db.dump()
+            req_type = req_range.twenty
+            page_range = req_type.val # will extend past this since we'll find new entries
+
+        # Check NSFW
+        current_page = 1
+        result_count = 1
+        # while we're under our page range and we havent checked every page
+        # check 1/3th of normal page range since NSFW entries are less common
+        while current_page <= math.ceil(
+                page_range / nsfw_div_factor) and result_count > 0:
+
+            logger.debug(f"[loop][NSFW] checking page {current_page}")
+            result_count, found_new_entries = add_from_page(
+                'nsfw', current_page)
+            page_range = update_req_range(found_new_entries, current_page, math.ceil(
+                page_range / nsfw_div_factor), is_nsfw=True)
+            current_page += 1
+
+        # Check SFW
+        current_page = 1
+        result_count = 1
+        page_range = req_type.val
+        # while we're under our page range and we havent checked every page
+        while current_page <= page_range and result_count > 0:
+            logger.debug(f"[loop][SFW] checking page {current_page}")
+            result_count, found_new_entries = add_from_page(
+                'sfw', current_page)
+            page_range = update_req_range(
+                found_new_entries, current_page, page_range, is_nsfw=False)
+            current_page += 1
+
+        # will only write req_range for sfw
+        last_scraped = [req_range.eight.last, req_range.twenty.last, req_range.all.last]
+
+        # at the time of writing this, there are 293 SFW pages
+        # its incredibly unlikely any approved entries will apear after page 250
+        # so its fine to use it as a magic number
+        # if we've checked all entries
+        if current_page > 250:
+            last_scraped[2] = int(time.time())
+        # if we extended past the thresholds because we found new entries
+        # we dont have to check for larger ranges when we typically would
+        if page_range >= req_range.twenty.val:
+            last_scraped[1] = int(time.time())
+        if page_range >= req_range.eight.val:
+            last_scraped[0] = int(time.time())
+
+        with open('state', 'w') as state_f:
+            state_f.write("\n".join(map(str, last_scraped)))
+
+        # sort id lists
+        db.lsort('sfw')
+        db.lsort('nsfw')
+
+        sleep_for = int(60 * random.uniform(27, 35))
+        logger.debug("Sleeping for {}m{}s".format(*divmod(sleep_for, 60)))
+        time.sleep(sleep_for)  # sleep for around 30 minutes
+
+
+def commit():
+    repo = Repo(root_dir)
+    if 'cache.json' in [i.a_path for i in repo.index.diff(None)]:
+        logger.debug("cache.json has been changed, commiting files and pushing")
+        repo.git.add('cache.json')
+        repo.index.commit("cache.json update")
+        origin = repo.remote(name='origin')
+        origin.push()
+    else:
+        logger.debug("cache.json is unchanged")
+
+if __name__ == "__main__":
+    cli()
