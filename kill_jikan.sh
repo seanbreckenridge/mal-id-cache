a5c456f14b435e6a533977454597c956efcbb84a
Sean Breckenridge <seanbrecke@gmail.com>
2019-07-21 23:14:11 -0700

    document

--- kill_jikan.sh
+++ kill_jikan.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# kills the jikan instance if it exists
 kill $(ps -e | grep jikan-rest/public | grep -v grep | cut -d" " -f 1)
--- start_jikan.sh
+++ start_jikan.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-ps -ef | grep -rin "jikan-rest/public" | grep -v "grep" || php -S localhost:8000 -t jikan-rest/public >> logs/jikan-requests.log 2>&1 &
+# if the jikan instance doesn't exist, start it
+
+ps -ef | grep "jikan-rest/public" | grep -v "grep" && echo "jikan is already running!" || php -S localhost:8000 -t jikan-rest/public >> logs/jikan-requests.log 2>&1 &
